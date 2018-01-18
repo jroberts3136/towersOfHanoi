4e45644bc5317b3d2dd0cba263133a857ced34df
jroberts3136 <Jroberts3136@gmail.com>
2018-01-18 08:38:38 -0500

    Tried to do some idiot proofing, it didn't work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.InputMismatchException;
+//import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,8 +15,28 @@ public class Main {
         String tower1 = "1";
         String tower2 = "2";
         String tower3 = "3";
+        //int temp;
         int diskNum = keyInput.nextInt();
-
+        /*boolean validDisk = false;
+        while(!validDisk) {
+            try {
+                temp = keyInput.nextInt();
+                if (temp > 0) {
+                    diskNum = temp;
+                    validDisk = true;
+                }
+                else{
+                    System.out.println("The number of disks must be a positive number.");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("You must enter the number of disks in number form.");
+            }
+            finally {
+                diskNum = 3;
+            }
+        }
+        */
         Tower hanoi = new Tower();
         hanoi.solve(diskNum, tower1, tower2, tower3);
     }
